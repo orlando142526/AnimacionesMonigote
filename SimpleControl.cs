eb3b58b029fab56b8b013f47a3d100f2f060dd51
orlando142526 <56253433+orlando142526@users.noreply.github.com>
2021-01-29 22:04:38 +0100

    Entrada-Salto-Entrada tecla de espacio

---
+++ SimpleControl.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.InputSystem;
+
+public class SimpleControl : MonoBehaviour
+{
+    Animator miAnimator;
+    bool Salto;
+
+    void Start()
+    {
+        miAnimator = GetComponent<Animator>();
+    }
+    private void OnFire(InputValue movementValue)
+    {
+        Debug.Log("Estoy en OnFire");
+        miAnimator.SetBool("Salto", true);
+    }
+
+        }
